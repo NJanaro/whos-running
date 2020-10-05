d1a1351663d4cddd8a3701323bcab6aff80a5e62
swatjoker <iSWATxJOKERi>
2020-10-05 12:13:55 -0400

    base frontend

---
+++ frontend/src/components/splash/splash.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Splash extends React.Component {
+    render() {
+        return (
+            <div>
+                <header>Who's Running?</header>
+            </div>
+        )
+    }
+}
+
+export default Splash;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Root from './components/root';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  ReactDOM.render(<Root />, root);
+});
+
+
