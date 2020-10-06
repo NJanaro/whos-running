2b5f03c770e9442a6878cf6bd4a6696cd081338b
Nick Janaro <nick.janaro@gmail.com>
2020-10-06 15:57:42 -0400

    chaning the internal packagejson

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+<<<<<<< HEAD
+=======
+app.get("/", (req, res) => res.render("index"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+>>>>>>> chaning the internal packagejson
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
