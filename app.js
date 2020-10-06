8b14cdae510c3be77e4e8ff34e04ab41e7764299
Nick Janaro <nick.janaro@gmail.com>
2020-10-05 21:19:15 -0400

    Working on getting this map up

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+// const express = require("express");
+const router = express.Router();
 
 const port = process.env.PORT || 5000;
 
@@ -7,3 +9,6 @@ app.get("/", (req, res) => res.send("Hello World, nice!! "));
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// app.get('/map', (req, res) => res.send("Hello World!"));
+
