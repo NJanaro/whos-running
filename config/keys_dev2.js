5a25d70ea7b83412ea8e2f7d1cc25380b6a437ae
Nick Janaro <nick.janaro@gmail.com>
2020-10-06 12:15:22 -0400

    getting rid of config in git

--- config/keys_dev2.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:0qyd9hAIr0m4VZnc@cluster.mongodb.net/test?retryWrites=true&w=majority",
-  googleAPI: "AIzaSyBtb748nfMJgTST6J-2WoOjLvadaRllRh8",
-  secretOrKey: "secret", // used for user auth later
-};
--- config/keys_prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    googleAPI: process.env.GOOGLE_API,
-    secretOrKey: process.env.SECRET_OR_KEY,
-};
-  
