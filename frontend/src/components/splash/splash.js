0b0ebe6de4b2375b40a591b1db3d22e2d18caf46
swatjoker <iSWATxJOKERi>
2020-10-05 20:34:44 -0400

    starting voter info

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import ElectionIndexItem from '../election/election_index_item';
 
 class Splash extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            address: ""
+            address: "",
+            splash_address: false,
+            election_id: null
         }
         this.handleInput = this.handleInput.bind(this);
+    }
+
+    componentDidMount() {
+        // debugger
+        this.props.fetchElections();
+    }
+
+    handleClick(id) {
+        this.setState({
+            election_id: id
+        })
     }
 
     handleInput(field) {
@@ -19,20 +33,27 @@ class Splash extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
+        const userInputAddy = [this.state.address, this.state.election_id];
 
     }
 
     render() {
+        let elections;
+        if(this.props.elections) {
+            elections = this.props.elections.map(election => {
+                return <ElectionIndexItem onClick={ this.handleClick(election.id) } key={ election.id } election={ election } />
+            })
+        }
         return (
-            <div className="center-splash">
-                <h1>Who's Running?</h1>
+            <div className={ this.state.splash_address ? "hide" : "splash" }>
+                <h1>Who's Running</h1>
                 <div className="splash-elections">
-                    
+                    { elections }
                 </div>
-                {/* <div className="search-box">
+                <div className={ this.state.splash_address ? "search-box" : "hide" }>
                     <input type="text" placeholder="Enter your address" onChange={ this.handleInput }></input>
-                    <button type="submit">Get Information</button>
-                </div> */}
+                    <button type="submit" onClick={ this.handleSubmit }>Get Information</button>
+                </div>
             </div>
         )
     }
--- frontend/src/util/elections_util.js
+++ frontend/src/util/elections_util.js
@@ -1,5 +1,18 @@
 import axios from 'axios';
+// import Key from '../../../config/key';
 
 export const getElections = () => {
-    return axios.get('https://www.googleapis.com/civicinfo/v2/elections?key=<YOUR_API_KEY>')
+    return axios.get(`https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyBtb748nfMJgTST6J-2WoOjLvadaRllRh8`)
+}
+
+export const getVoterInfo = address => {
+    return axios({
+        method: 'get',
+        url: 'https://www.googleapis.com/civicinfo/v2/voterinfo',
+        params: {
+            key: 'AIzaSyBtb748nfMJgTST6J-2WoOjLvadaRllRh8',
+            address: address[0],
+            electionId: address[1]
+        }
+    })
 }
