2c6e2aac02757a83f12051552d32121901cd4a1d
Nick Janaro <nick.janaro@gmail.com>
2020-10-06 22:52:09 -0400

    checking production

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import Root from './components/root';
 import { getElections } from './util/elections_util';
 import configureStore from './store/store';
+import axios from 'axios';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
