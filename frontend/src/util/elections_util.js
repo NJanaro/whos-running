6845d388bc7c431bb284d541cb78f3c446a0e1aa
Nick Janaro <nick.janaro@gmail.com>
2020-10-07 00:48:19 -0400

    One more time

--- frontend/src/util/elections_util.js
+++ frontend/src/util/elections_util.js
@@ -6,6 +6,7 @@ export const getElections = () => {
 }
 
 export const getVoterInfo = address => {
+    console.log(Key.googleAPI);
     return axios({
         method: 'get',
         url: 'https://www.googleapis.com/civicinfo/v2/voterinfo',
