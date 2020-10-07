0afb8f4c6d92fd333daf50720da93cd6e8144b5c
Nick Janaro <nick.janaro@gmail.com>
2020-10-07 00:30:36 -0400

    just kidding

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 // const router = express.Router();
+
+// app.set("view engine", "ejs");
+const port = process.env.PORT || 5000;
+
+// app.use(express.static(__dirname + "../public"));
+// app.set('views', path.join(__dirname, '/frontend/public'));
+// app.get("/", (req, res) => res.render('index'));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
@@ -9,10 +18,3 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
-
