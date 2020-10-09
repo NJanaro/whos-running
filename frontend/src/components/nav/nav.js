7c1b1ac7c06f2d89d9f8666b36adb47619562fff
Nick Janaro <nick.janaro@gmail.com>
2020-10-09 11:50:06 -0400

    should have been working in maps

--- frontend/src/components/nav/nav.js
+++ frontend/src/components/nav/nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../../stylesheets/components/_nav.scss';
+import vote from '../../img/bigvote.png';
+
 
 class Navbar extends React.Component {
     // constructor(props) {
@@ -9,11 +11,28 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            <section className="navbar">
-                <li className="home-btn" onClick={ () => this.props.history.push("/") }>Home</li>
-                <li className="representatives-btn" onClick={ () => this.props.history.push("/representatives") }>Representatives</li>
-            </section>
-        )
+          <>
+            <div className="navbar">
+              <div className="nav-left">
+                <li
+                  className="home-btn"
+                  onClick={() => this.props.history.push("/")}
+                >
+                  Home
+                </li>
+                <li
+                  className="representatives-btn"
+                  onClick={() => this.props.history.push("/representatives")}
+                >
+                  Representatives
+                </li>
+              </div>
+              <div className="nav-right">
+                <img id="nav-vote" src={vote} alt="vote-logo"></img>
+              </div>
+            </div>
+          </>
+        );
     }
 }
 
--- frontend/src/components/user_quering/representatives_index_item.js
+++ frontend/src/components/user_quering/representatives_index_item.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import OfficialModal from './official_modal';
+import infoIcon from '../../img/info.png';
 
 class RepresentativesIndexItem extends React.Component {
     constructor(props) {
@@ -22,32 +23,80 @@ class RepresentativesIndexItem extends React.Component {
         // debugger
         // 1660 Topping Ave Bronx NY 10457
         return (
-            <>
-                <span onClick={ this.toggleModal } className={ this.state.modal ? "close" : "hide" } >&times;</span>
-                <div onClick={ this.toggleModal } className="official-info">
-                    <li>{ this.props.official.name }</li>
-                    <li>{ this.props.official.party }</li>
-                    <li>{ this.props.name ? this.props.name : "" } </li>
-                </div>
-                <div className="address-box">
-                    <li>{ this.props.official.address ? this.props.official.address[0].line1 : ""}</li>
-                    <li>{ this.props.official.address ? this.props.official.address[0].line2 : ""}</li>
-                    <li>{ this.props.official.address ? this.props.official.address[0].city + ', ' + this.props.official.address[0].state : ""}</li>
-                    <li>{ this.props.official.address ? this.props.official.address[0].zip : ""}</li>
-                </div>
-                <div className="contact-info">
-                    <li>{ this.props.official.emails ? this.props.official.emails.map(email => email) : "" }</li>
-                    <li>{ this.props.official.phones ? this.props.official.phones.map(number => number) : "" }</li>
-                    { this.props.official.urls ? this.props.official.urls.map((url,idx) => {
-                        return <a key={ idx } href={ url }>Website</a>
-                        }) : ""
-                    }
-                </div>
-                <section className={ this.state.modal ? "modal" : "hide" }>
-                    <OfficialModal official={ this.props.official } modal={ this.state.modal } repsInfo={ this.props.repsInfo } name={ this.props.name }/>
-                </section>
-            </>
-        )
+          <>
+            <span
+              onClick={this.toggleModal}
+              className={this.state.modal ? "close" : "hide"}
+            >
+              &times;
+            </span>
+            <div onClick={this.toggleModal} className="info-icon-box">
+              <img id="info-icon" src={infoIcon} alt="info"></img>
+            </div>
+            <div className="official-info">
+              <ul>
+                <li>Name: {this.props.official.name}</li>
+                <li className={this.props.official.party.slice(0, 1)}>
+                  Party: {this.props.official.party}
+                </li>
+                <li>Position: {this.props.name ? this.props.name : ""} </li>
+              </ul>
+            </div>
+            <div className="address-box">
+              <li>
+                {this.props.official.address
+                  ? this.props.official.address[0].line1
+                  : ""}
+              </li>
+              <li>
+                {this.props.official.address
+                  ? this.props.official.address[0].line2
+                  : ""}
+              </li>
+              <li>
+                {this.props.official.address
+                  ? this.props.official.address[0].city +
+                    ", " +
+                    this.props.official.address[0].state
+                  : ""}
+              </li>
+              <li>
+                {this.props.official.address
+                  ? this.props.official.address[0].zip
+                  : ""}
+              </li>
+            </div>
+            <div className="contact-info">
+              <li>
+                {this.props.official.emails
+                  ? this.props.official.emails.map((email) => email)
+                  : ""}
+              </li>
+              <li>
+                {this.props.official.phones
+                  ? this.props.official.phones.map((number) => number)
+                  : ""}
+              </li>
+              {this.props.official.urls
+                ? this.props.official.urls.map((url, idx) => {
+                    return (
+                      <a key={idx} href={url}>
+                        Website
+                      </a>
+                    );
+                  })
+                : ""}
+            </div>
+            <section className={this.state.modal ? "modal" : "hide"}>
+              <OfficialModal
+                official={this.props.official}
+                modal={this.state.modal}
+                repsInfo={this.props.repsInfo}
+                name={this.props.name}
+              />
+            </section>
+          </>
+        );
     }
 }
 
